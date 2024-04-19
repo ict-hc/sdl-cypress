497e16b8bf52fef2e4361ded9affc7a4d586925b
Brayan Vargas <brayan.vargas@promedital.it>
2024-04-19 16:51:27 +0200

    Update machine creation form with additional fields

--- cypress/e2e/machines.cy.js
+++ cypress/e2e/machines.cy.js
@@ -22,14 +22,29 @@ describe("E2E Machines", function () {
       ".ant-row > .ant-col > .ant-form-item-control-input > .ant-form-item-control-input-content > #main-form_serial";
     this.pointer =
       ".ant-form-item-control-input > .ant-form-item-control-input-content > .ant-input-number > .ant-input-number-input-wrap > #main-form_pointer";
+    this.area = "#rc_select_2";
+    this.label = "#rc_select_3";
+    this.labelPrinter = "#rc_select_4";
 
     cy.get(this.code).click().type("MACHINE_EXAMPLE_1");
     cy.get(this.address).click().type("127.0.0.1");
+
     cy.get(this.machineType).type("BP100");
     cy.get("[title = 'BP100']").click();
+
     cy.get(this.port).click().type("80");
     cy.get(this.serial).click().type("2301710EE104A");
-    cy.get(this.pointer).click().type("1");
+
+    cy.get(this.area).click();
+    cy.get("[title = 'A01 - Default Area']").click();
+
+    cy.get(this.label).click();
+    cy.get("[title = 'A01 - Default label']").click();
+
+    cy.get(this.labelPrinter).click();
+    cy.get("[title = 'A01']").click();
+
+    cy.get(this.pointer).type("1");
 
     cy.get("#topbar button")
       .should("be.visible")
@@ -124,5 +139,14 @@ describe("E2E Machines", function () {
       "not.contain",
       "MACHINE_EXAMPLE_1_CHANGED"
     );
+  });
+
+  it("Make sure that fields in Creation are equal to 10", function () {
+    cy.wait(2000);
+    cy.visit("/machines/new");
+    cy.url().should("include", "/machines/new");
+    cy.wait(500);
+
+    cy.get("form input").should("have.length", 10);
   });
 });
